a94dbb385e2aed79d557f806e5ac11e5a79cfe93
Rick <rmt1998@hotmail.nl>
2018-07-08 23:15:14 +0200

    Version 0.1

--- app/src/main/java/com/rickteuthof/strangejourneycompendium/Demon.java
+++ app/src/main/java/com/rickteuthof/strangejourneycompendium/Demon.java
@@ -1,8 +1,174 @@
 package com.rickteuthof.strangejourneycompendium;
 
-import android.arch.persistence.room.Entity;
+import java.util.ArrayList;
 
-@Entity
-public class Demon {
+class Attack
+{
+    private String target;
 
+    public String getTarget() { return this.target; }
+
+    public void setTarget(String target) { this.target = target; }
+
+    private String ailment;
+
+    public String getAilment() { return this.ailment; }
+
+    public void setAilment(String ailment) { this.ailment = ailment; }
+
+    private String hits;
+
+    public String getHits() { return this.hits; }
+
+    public void setHits(String hits) { this.hits = hits; }
+
+    private String element;
+
+    public String getElement() { return this.element; }
+
+    public void setElement(String element) { this.element = element; }
+}
+
+class Reslvls
+{
+    private int fir;
+
+    public int getFir() { return this.fir; }
+
+    public void setFir(int fir) { this.fir = fir; }
+
+    private int ele;
+
+    public int getEle() { return this.ele; }
+
+    public void setEle(int ele) { this.ele = ele; }
+
+    private int gun;
+
+    public int getGun() { return this.gun; }
+
+    public void setGun(int gun) { this.gun = gun; }
+
+    private int ice;
+
+    public int getIce() { return this.ice; }
+
+    public void setIce(int ice) { this.ice = ice; }
+
+    private int win;
+
+    public int getWin() { return this.win; }
+
+    public void setWin(int win) { this.win = win; }
+
+    private int phy;
+
+    public int getPhy() { return this.phy; }
+
+    public void setPhy(int phy) { this.phy = phy; }
+
+    private int cur;
+
+    public int getCur() { return this.cur; }
+
+    public void setCur(int cur) { this.cur = cur; }
+
+    private int exp;
+
+    public int getExp() { return this.exp; }
+
+    public void setExp(int exp) { this.exp = exp; }
+}
+
+public class Demon
+{
+    private String name;
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
+
+    private String align;
+
+    public String getAlign() { return this.align; }
+
+    public void setAlign(String align) { this.align = align; }
+
+    private Attack attack;
+
+    public Attack getAttack() { return this.attack; }
+
+    public void setAttack(Attack attack) { this.attack = attack; }
+
+    private int code;
+
+    public int getCode() { return this.code; }
+
+    public void setCode(int code) { this.code = code; }
+
+    private String inherits;
+
+    public String getInherits() { return this.inherits; }
+
+    public void setInherits(String inherits) { this.inherits = inherits; }
+
+    private double lvl;
+
+    public double getLvl() { return this.lvl; }
+
+    public void setLvl(double lvl) { this.lvl = lvl; }
+
+    private int pcoeff;
+
+    public int getPcoeff() { return this.pcoeff; }
+
+    public void setPcoeff(int pcoeff) { this.pcoeff = pcoeff; }
+
+    private String race;
+
+    public String getRace() { return this.race; }
+
+    public void setRace(String race) { this.race = race; }
+
+    private String resists;
+
+    public String getResists() { return this.resists; }
+
+    public void setResists(String resists) { this.resists = resists; }
+
+    private ArrayList<String> skills;
+
+    public ArrayList<String> getSkills() { return this.skills; }
+
+    public void setSkills(ArrayList<String> skills) { this.skills = skills; }
+
+    private ArrayList<String> source;
+
+    public ArrayList<String> getSource() { return this.source; }
+
+    public void setSource(ArrayList<String> source) { this.source = source; }
+
+    private ArrayList<Integer> stats;
+
+    public ArrayList<Integer> getStats() { return this.stats; }
+
+    public void setStats(ArrayList<Integer> stats) { this.stats = stats; }
+
+    private String ailments;
+
+    public String getAilments() { return this.ailments; }
+
+    public void setAilments(String ailments) { this.ailments = ailments; }
+
+    private Reslvls reslvls;
+
+    public Reslvls getReslvls() { return this.reslvls; }
+
+    public void setReslvls(Reslvls reslvls) { this.reslvls = reslvls; }
+
+    private Double hpmod;
+
+    public Double getHpmod() { return this.hpmod; }
+
+    public void setHpmod(Double hpmod) { this.hpmod = hpmod; }
 }
--- app/src/main/java/com/rickteuthof/strangejourneycompendium/MainActivity.java
+++ app/src/main/java/com/rickteuthof/strangejourneycompendium/MainActivity.java
@@ -1,7 +1,22 @@
 package com.rickteuthof.strangejourneycompendium;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BufferedReader bufferedReader = null;
+        InputStream is = getResources().openRawResource(R.raw.demon_data);
+        Reader r = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(r);
+        Gson gson = new Gson();
+        Demon[] demons = gson.fromJson(br, Demon[].class);
+
+        TextView t=new TextView(this);
+
+        t=(TextView)findViewById(R.id.test);
+        t.setText("Demon 0 name is: " + demons[0].getName());
     }
 }
