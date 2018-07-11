77dbcc580055c2928f4d20bfdf268739708fe17f
Rick Teuthof <rmt1998@hotmail.nl>
2018-07-11 16:29:44 +0200

    Added inheritance

--- app/src/main/java/com/rickteuthof/strangejourneycompendium/Parsers.java
+++ app/src/main/java/com/rickteuthof/strangejourneycompendium/Parsers.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class Parsers {
     public static ArrayList<String> parseResistance(String resists) {
-        String[] elements = {"phys: ", "gun: ", "fire: ", "ice: ", "elec: ", "wind: ", "expel: ", "curse: "};
+        String[] elements = {"phys: ", "gun: ", "fire: ", "ice: ", "elec: ", "wind: ", "expel: ",
+                "curse: "};
         ArrayList<String> parsed = new ArrayList<>(8);
         for (int i = 0; i < 8; i++) {
             String curElement = elements[i];
@@ -35,8 +36,13 @@ public class Parsers {
     }
 
     public static ArrayList<String> parseAilments(String ailmentRes) {
-        String[] ailments = {"poison: ", "paralyze: ", "stone: ", "strain: ", "sleep: ", "charm: ", "mute: ", "fear: ", "bomb: "};
         ArrayList<String> parsed = new ArrayList<>(9);
+        if (ailmentRes == null) {
+            parsed.add("no special resistances");
+            return parsed;
+        }
+        String[] ailments = {"poison: ", "paralyze: ", "stone: ", "strain: ", "sleep: ", "charm: ",
+                "mute: ", "fear: ", "bomb: "};
         for (int i = 0; i < 9; i++) {
             String curAilment = ailments[i];
             char cur = ailmentRes.charAt(i);
@@ -54,6 +60,23 @@ public class Parsers {
                     parsed.add(curAilment + "-");
                     break;
             }
+        }
+
+        return parsed;
+    }
+
+    public static ArrayList<String> parseInherits(String inherits) {
+        ArrayList<String> parsed = new ArrayList<>();
+        String[] types = {"special", "fire", "ice", "elec", "wind", "expel", "curse", "almighty",
+                "phys", "gun", "ailments", "life", "mana", "support", "recover"};
+        for (int i = 0; i < 14; i++) {
+            if (inherits.charAt(i) == 'o') {
+                parsed.add(types[i]);
+            }
+        }
+
+        if (parsed.size() == 0) {
+            parsed.add("none");
         }
 
         return parsed;
